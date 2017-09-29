9ef558d21544d37f3ae578981ccf5eebf2419c6b
chetan-fueled <chetan@fueled.com>
2017-09-29 11:05:25 +0530

    refactor(RecyclerViewScrollCallback

--- app/src/main/java/com/fueled/recyclerviewbindings/widget/scroll/RecyclerViewScrollCallback.kt
+++ app/src/main/java/com/fueled/recyclerviewbindings/widget/scroll/RecyclerViewScrollCallback.kt
@@ -1,7 +1,9 @@
 package com.fueled.recyclerviewbindings.core
 
+import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.StaggeredGridLayoutManager
 
 class RecyclerViewScrollCallback private constructor(private val visibleThreshold: Int, private val layoutManager: RecyclerView.LayoutManager) : RecyclerView.OnScrollListener() {
     // The minimum amount of items to have below your current scroll position
@@ -40,25 +42,55 @@ class RecyclerViewScrollCallback private constructor(private val visibleThreshol
     // We are given a few useful parameters to help us work out if we need to load some more data,
     // but first we check if we are waiting for the previous load to finish.
     override fun onScrolled(view: RecyclerView, dx: Int, dy: Int) {
-        var visibleItemCount = view.getChildCount()
-        var totalItemCount = layoutManager.getItemCount()
-        var firstVisibleItem = (layoutManager as LinearLayoutManager).findFirstVisibleItemPosition()
+        var lastVisibleItemPosition = 0
+        val totalItemCount = layoutManager.itemCount
 
-        if (loading) {
-            if (totalItemCount > previousTotalItemCount) {
-                loading = false;
-                previousTotalItemCount = totalItemCount;
+        when (layoutManager) {
+            is StaggeredGridLayoutManager -> {
+                val lastVisibleItemPositions = layoutManager.findLastVisibleItemPositions(null)
+                // get maximum element within the list
+                lastVisibleItemPosition = getLastVisibleItem(lastVisibleItemPositions)
+            }
+            is GridLayoutManager -> lastVisibleItemPosition = layoutManager.findLastVisibleItemPosition()
+            is LinearLayoutManager -> lastVisibleItemPosition = layoutManager.findLastVisibleItemPosition()
+        }
+
+        // If the total item count is zero and the previous isn't, assume the
+        // list is invalidated and should be reset back to initial state
+        // If it’s still loading, we check to see if the dataset count has
+        // changed, if so we conclude it has finished loading and update the current page
+        // number and total item count.
+        // Using >= instead of > because when swiped, LayoutManager shouldn't clear the list
+
+        // If it isn’t currently loading, we check to see if we have breached
+        // the visibleThreshold and need to reload more data.
+        // If we do need to reload some more data, we execute onScrolledListener to fetch the data.
+        // threshold should reflect how many total columns there are too
+
+        // If the total item count is zero and the previous isn't, assume the
+        // list is invalidated and should be reset back to initial state
+        if (totalItemCount < previousTotalItemCount) {
+            this.currentPage = this.startingPageIndex
+            this.previousTotalItemCount = totalItemCount
+            if (totalItemCount == 0) {
+                this.loading = true
             }
         }
-        if (!loading && (totalItemCount - visibleItemCount)
-                <= (firstVisibleItem + visibleThreshold)) {
-            // End has been reached
+        // If it’s still loading, we check to see if the dataset count has
+        // changed, if so we conclude it has finished loading and update the current page
+        // number and total item count.
+        if (loading && totalItemCount > previousTotalItemCount) {
+            loading = false
+            previousTotalItemCount = totalItemCount
+        }
 
-            // Do something
+        // If it isn’t currently loading, we check to see if we have breached
+        // the visibleThreshold and need to reload more data.
+        // If we do need to reload some more data, we execute onScrolledListener to fetch the data.
+        // threshold should reflect how many total columns there are too
+        if (!loading && lastVisibleItemPosition + visibleThreshold > totalItemCount) {
             currentPage++
-
             onScrolledListener.onScrolledToBottom(currentPage)
-
             loading = true
         }
     }
